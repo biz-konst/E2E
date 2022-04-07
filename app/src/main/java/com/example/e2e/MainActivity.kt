36a1d769ed9cfe585c47717a610ba05aea905b42
tiss-develop <tiss6278git>
2022-04-07 17:21:17 +0700

    update

--- app/src/main/java/com/example/e2e/MainActivity.kt
+++ app/src/main/java/com/example/e2e/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.e2e
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // включаем рисование за системными барами
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // далее устанавливаем цвета статус-бара и бара навигации в xml (api >= 21)
+        // android:statusBarColor, android:navigationBarColor
+        // (либо включаем android:windowTranslucentStatus, android:windowTranslucentNavigation для api 19-20)
+        // для светлого фона дополнительно включаем флаги android:windowLightStatusBar (api > 23),
+        // android:windowLightNavigationBar (api >= 27)
+        // если api < 27 (api < 23 для статус-бара) и светлый фон, можно установить полупрозрачные цвета,
+        // или флаги windowTranslucentXXX
+
         setContentView(R.layout.activity_main)
     }
 }
